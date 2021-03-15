4344ae09ec7b87ef677f55fce487140c00e037e5
George Papagapitos <george.g.papagapitos@gmail.com>
2021-03-15 14:46:07 -0500

    postman tests working

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -20,11 +20,15 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 router.post('/register', (req, res, next) => {
   const username = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const age = req.body.age;
+  const readingGradeLevel = req.body.readingGradeLevel;
 
-  const queryText = `INSERT INTO "user" (username, password)
-    VALUES ($1, $2) RETURNING id`;
+  const queryText = `INSERT INTO "users" (username, password, first_name, last_name, age, reading_grade_level)
+    VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`;
   pool
-    .query(queryText, [username, password])
+    .query(queryText, [username, password, firstName, lastName, age, readingGradeLevel])
     .then(() => res.sendStatus(201))
     .catch((err) => {
       console.log('User registration failed: ', err);
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -2,20 +2,32 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 function RegisterForm() {
+  
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [age, setAge] = useState('');
+  const [readingGradeLevel, setReadingGradeLevel]= useState('');
+
   const errors = useSelector((store) => store.errors);
   const dispatch = useDispatch();
+
+  const userToDispatch = {
+    username,
+    password,
+    firstName,
+    lastName,
+    age,
+    readingGradeLevel
+  }
 
   const registerUser = (event) => {
     event.preventDefault();
 
     dispatch({
       type: 'REGISTER',
-      payload: {
-        username: username,
-        password: password,
-      },
+      payload: userToDispatch
     });
   }; // end registerUser
 
